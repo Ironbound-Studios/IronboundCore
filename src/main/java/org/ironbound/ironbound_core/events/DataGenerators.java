aedfa5ad6270d6014331a1fbd7b19b79048e8715
clcment446 <125682659+clcment446@users.noreply.github.com>
2024-11-05 16:29:07 +0100

    change build.gradle, remove arcanus dep

--- src/main/java/org/ironbound/ironbound_core/events/DataGenerators.java
+++ src/main/java/org/ironbound/ironbound_core/events/DataGenerators.java
@@ -7,7 +7,7 @@ import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.data.event.GatherDataEvent;
 import org.ironbound.ironbound_core.Ironbound;
-import org.ironbound.ironbound_core.datagen.IronboundRitualRecipeProvider;
+//import org.ironbound.ironbound_core.datagen.IronboundRitualRecipeProvider;
 
 import java.util.concurrent.CompletableFuture;
 
@@ -19,9 +19,9 @@ public class DataGenerators {
         PackOutput output = generator.getPackOutput();
         CompletableFuture<HolderLookup.Provider> lookupProvider = event.getLookupProvider();
         // other providers here
-        generator.addProvider(
+/*        generator.addProvider(
                 event.includeServer(),
                 new IronboundRitualRecipeProvider(output, lookupProvider)
-        );
+        );*/
     }
 }
--- src/main/java/org/ironbound/ironbound_core/util/IronboundCoreEffect.java
+++ src/main/java/org/ironbound/ironbound_core/util/IronboundCoreEffect.java
@@ -15,6 +15,9 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.attributes.Attribute;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.SwordItem;
+import net.minecraft.world.item.Tier;
+import net.minecraft.world.item.Tiers;
 import org.ironbound.ironbound_core.registries.IBDamageSourcesReg;
 import org.jetbrains.annotations.NotNull;
 
